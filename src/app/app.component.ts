fc9062568a519d39d9006a43186909d7bedcc63f
hpr-nms <54054583+hpr-nms@users.noreply.github.com>
2019-10-02 23:40:16 +0330

    angular material
    app component mat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, Input, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { IdxOptions } from './shared/components/dx-components/dx-base/IdxOptions';
+import { MatInput } from '@angular/material/input';
+import { FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
-
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 declare var _: any;
 // declare var Storages: any;
@@ -11,12 +20,21 @@ declare var fakeLoader: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 
   // providers: [MousetrapService, CustomEventsService]
 })
 export class AppComponent implements OnInit {
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  @Input() username: MatInput;
   public layout = 'top-navigation-1'; // default-sidebar-1,off-canvas-1,sidebar-over-1,off-canvas-1,
   // top-navigation-1,top-navigation-2,empty-view-1
   public background = 'light'; // light,dark,indigo,blue-grey
@@ -29,7 +47,7 @@ export class AppComponent implements OnInit {
   public view: string;
   defultContextmenu;
   public leftBoxParvande = -615;
-
+  name;
   isOpenKey = false;
 
   ////////////////
@@ -42,67 +60,77 @@ export class AppComponent implements OnInit {
     private router: Router
   ) { this.defultContextmenu = true; }
 
-  MoveUp() {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
-  @HostListener('window:scroll')
-  onScroll() {
-    if (document.getElementById('myBtn')) {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        document.getElementById('myBtn').style.display = 'block';
-      } else {
-        document.getElementById('myBtn').style.display = 'none';
-      }
+
+
+hos(event){
+  console.log(event);
+  // this.inp.focus();
+}
+hhh(checkoutForm: NgForm){
+  console.log(checkoutForm);
+  // checkoutForm.reset();
+}
+MoveUp() {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+@HostListener('window:scroll')
+onScroll() {
+  if (document.getElementById('myBtn')) {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById('myBtn').style.display = 'block';
+    } else {
+      document.getElementById('myBtn').style.display = 'none';
     }
   }
-  ngOnInit() {
+}
+ngOnInit() {
 
-    const body = document.getElementsByTagName('body')[0];
-    body.setAttribute('oncontextmenu', 'return ' + this.defultContextmenu);
+  const body = document.getElementsByTagName('body')[0];
+  body.setAttribute('oncontextmenu', 'return ' + this.defultContextmenu);
 
 
 
-    this.Options = new IdxOptions();
-    this.getFormList();
-  }
+  this.Options = new IdxOptions();
+  this.getFormList();
+}
   private getFormList() {
-    this.dataSource = [
-      // tslint:disable-next-line: max-line-length
-      { id: 1, name: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
-      // tslint:disable-next-line: max-line-length
-      { id: 2, name: 'لورم ایپسوم متن ساختگی با تولید سادگیرها و متوبرای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
-      // tslint:disable-next-line: max-line-length
-      { id: 3, name: 'لورم ایپسوم متن ساخی متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
-      { id: 4, name: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ا می طلبد تا ' }
-    ]
-    this.Options.Columns = [
-      { caption: 'ایدی', dataField: 'id', dataType: "string" },
-      { caption: 'نام بانک', dataField: 'name', dataType: "string" }
-    ];
-    this.Options.sorting = { allowSorting: true, sortingMode: 'multiple' }
+  this.dataSource = [
+    // tslint:disable-next-line: max-line-length
+    { id: 1, name: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
+    // tslint:disable-next-line: max-line-length
+    { id: 2, name: 'لورم ایپسوم متن ساختگی با تولید سادگیرها و متوبرای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
+    // tslint:disable-next-line: max-line-length
+    { id: 3, name: 'لورم ایپسوم متن ساخی متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا ' },
+    { id: 4, name: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ا می طلبد تا ' }
+  ]
+  this.Options.Columns = [
+    { caption: 'ایدی', dataField: 'id', dataType: "string" },
+    { caption: 'نام بانک', dataField: 'name', dataType: "string" }
+  ];
+  this.Options.sorting = { allowSorting: true, sortingMode: 'multiple' }
 
-  }
-
-
+}
 
 
-  getserver() {
-    this.getFormList();
-  }
 
-  submitForm(MapParvaneForm, $event) {
 
-  }
-  clearform(form) {
+getserver() {
+  this.getFormList();
+}
 
-  }
+submitForm(MapParvaneForm, $event) {
+
+}
+clearform(form) {
+
+}
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import {
   CUSTOM_ELEMENTS_SCHEMA
 } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { AppComponent } from './app.component';
 import { SharedModuleModule } from './shared-module/shared-module.module';
 import { AppRoutingModule } from './app-routing.module';
 
-
-
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { DxModuleModule } from './shared/components/dx-module/dx-module.module';
 import { DxService } from './shared/components/dx-components/dx.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent],
@@ -26,8 +28,12 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     FormsModule,
     DxModuleModule,
-
-    AppRoutingModule
+    MatInputModule,
+    MatButtonModule,MatDatepickerModule,
+    AppRoutingModule,MatNativeDateModule,MatCheckboxModule,
+    ReactiveFormsModule,
+    // MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DxService
